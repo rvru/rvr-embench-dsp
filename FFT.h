8a73070305261d322bf7078100701a2b2ddbf6aa
AndyLu98 <37010236+AndyLu98@users.noreply.github.com>
2020-12-20 21:17:36 -0500

    Add files via upload

---
+++ FFT.h
@@ -0,0 +1,5 @@
+int fft_dif(double x[], double y[], int n, int m);
+
+int fft_dit(double x[], double y[], int n, int m);
+
+int check_if_equal(double input_x[], double input_y[], double expected_x[], double expected_y[], int data_length, float epsilon);                       
