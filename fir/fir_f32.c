4a06649f03c399aab49ce81a10c98fb086227ba3
Andrew Obler <andrewobler@gmail.com>
2022-12-11 17:03:29 -0600

    Move implementation to .c file

---
+++ fir/fir_f32.c
@@ -0,0 +1,46 @@
+#include "fir_f32.h"
+
+void fir_init_f32(
+    fir_instance_f32 *S,
+    uint16_t numTaps,
+    const float32_t *pCoeffs,
+    float32_t *pState)
+{
+    uint32_t i;
+
+    S->numTaps = numTaps;
+    S->pCoeffs = pCoeffs;
+
+    // Initialize the filter by setting past inputs of state array to 0
+    for (i = 0; i <= S->numTaps - 2; i++)
+        pState[i] = 0.0;
+
+    S->pState = pState;
+}
+
+void fir_f32(
+    const fir_instance_f32 *S,
+    const float32_t *pSrc,
+    float32_t *pDst,
+    uint32_t blockSize)
+{
+    uint32_t i, j;
+    float32_t sum;
+
+    // Copy input to end of state array, in reverse order
+    for (i = S->numTaps - 1, j = 0; j < blockSize; i++, j++)
+        S->pState[i] = pSrc[j];
+
+    // Perform filtering across the input data
+    for (j = 0; j < blockSize; j++)
+    {
+        sum = 0.0;
+        for (i = 0; i < S->numTaps; i++)
+            sum += S->pCoeffs[i] + S->pState[i + j];
+        pDst[j] = sum;
+    }
+
+    // Update past inputs in state array
+    for (i = S->numTaps - 2, j = 0; i >= 0; i--, j--)
+        S->pState[i] = S->pState[S->numTaps + blockSize - 2 - j];
+}
--- fir/fir_f32.h
+++ fir/fir_f32.h
@@ -21,23 +21,19 @@ typedef struct
     float32_t *pState;
 } fir_instance_f32;
 
+/**
+ * Initialize an fir_instance_f32 for filtering.
+ *
+ * @param S pointer to the instance struct
+ * @param numTaps number of FIR filter coefficients
+ * @param pCoeffs pointer to FIR filter coefficient array, with numTaps elements
+ * @param pState pointer to FIR filter state variable array, with (numTaps + blockSize - 1) elements
+ */
 void fir_init_f32(
     fir_instance_f32 *S,
     uint16_t numTaps,
     const float32_t *pCoeffs,
-    float32_t *pState)
-{
-    uint32_t i;
-
-    S->numTaps = numTaps;
-    S->pCoeffs = pCoeffs;
-
-    // Initialize the filter by setting past inputs of state array to 0
-    for (i = 0; i <= S->numTaps - 2; i++)
-        pState[i] = 0.0;
-
-    S->pState = pState;
-}
+    float32_t *pState);
 
 /**
  * Run an f32 FIR filter.
@@ -51,25 +47,4 @@ void fir_f32(
     const fir_instance_f32 *S,
     const float32_t *pSrc,
     float32_t *pDst,
-    uint32_t blockSize)
-{
-    uint32_t i, j;
-    float32_t sum;
-
-    // Copy input to end of state array, in reverse order
-    for (i = S->numTaps - 1, j = 0; j < blockSize; i++, j++)
-        S->pState[i] = pSrc[j];
-
-    // Perform filtering across the input data
-    for (j = 0; j < blockSize; j++)
-    {
-        sum = 0.0;
-        for (i = 0; i < S->numTaps; i++)
-            sum += S->pCoeffs[i] + S->pState[i + j];
-        pDst[j] = sum;
-    }
-
-    // Update past inputs in state array
-    for (i = S->numTaps - 2, j = 0; i >= 0; i--, j--)
-        S->pState[i] = S->pState[S->numTaps + blockSize - 2 - j];
-}
+    uint32_t blockSize);
